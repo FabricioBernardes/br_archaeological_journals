041473a56c3a7576132ba3476c5111f1b1df99ad
fabricio.bernardes <fabriciob@vakinha.com.br>
2025-05-19 09:59:12 -0300

    feat: add scientific journals data to seeds file for initial database population

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,44 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+scientific_journals = [
+  {
+    name: "Revista Brasileira de Arqueologia",
+    institutional_affiliation: "Sociedade de Arqueologia Brasileira",
+    issn: "0102-8650",
+    thematic_scope: "Arqueologia brasileira e internacional",
+    website_url: "https://revista.sabnet.org/ojs/index.php/sab",
+    periodicity: "Semestral",
+    current_status: "Ativa",
+    foundation_year: 1981,
+    closure_year: nil,
+    qualis: "A2"
+  },
+  {
+    name: "Clio Arqueológica",
+    institutional_affiliation: "Universidade Federal de Pernambuco",
+    issn: "0102-6003",
+    thematic_scope: "Arqueologia, patrimônio e história",
+    website_url: "https://periodicos.ufpe.br/revistas/clioarqueologica",
+    periodicity: "Semestral",
+    current_status: "Ativa",
+    foundation_year: 1984,
+    closure_year: nil,
+    qualis: "B1"
+  },
+  {
+    name: "Boletim do Museu Paraense Emílio Goeldi. Ciências Humanas",
+    institutional_affiliation: "Museu Paraense Emílio Goeldi",
+    issn: "1981-8122",
+    thematic_scope: "Antropologia, arqueologia, etnologia",
+    website_url: "http://editora.museu-goeldi.br/humanas",
+    periodicity: "Quadrimestral",
+    current_status: "Ativa",
+    foundation_year: 2006,
+    closure_year: nil,
+    qualis: "A1"
+  }
+]
+
+scientific_journals.each do |attrs|
+  ScientificJournal.find_or_create_by!(issn: attrs[:issn]) do |journal|
+    journal.assign_attributes(attrs)
+  end
+end

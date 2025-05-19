cc27fdbb955a58ab17e510e36e11b0afcfa469e5
fabricio.bernardes <fabriciob@vakinha.com.br>
2025-05-19 10:35:52 -0300

    feat: add keywords model and associations with articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :edition
+  has_and_belongs_to_many :keywords
 
   validates :title, presence: true
   validates :authors, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_19_132314) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_19_133229) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,13 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_19_132314) do
     t.index ["edition_id"], name: "index_articles_on_edition_id"
   end
 
+  create_table "articles_keywords", id: false, force: :cascade do |t|
+    t.bigint "article_id", null: false
+    t.bigint "keyword_id", null: false
+    t.index ["article_id", "keyword_id"], name: "index_articles_keywords_on_article_id_and_keyword_id"
+    t.index ["keyword_id", "article_id"], name: "index_articles_keywords_on_keyword_id_and_article_id"
+  end
+
   create_table "editions", force: :cascade do |t|
     t.string "volume"
     t.date "publication_date"
@@ -51,6 +58,12 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_19_132314) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["scientific_journal_id"], name: "index_editions_on_scientific_journal_id"
+  end
+
+  create_table "keywords", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "scientific_journals", force: :cascade do |t|

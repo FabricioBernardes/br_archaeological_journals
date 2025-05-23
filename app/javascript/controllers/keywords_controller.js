d45b69d8a521a2f9d575d64c0407267a65a72ba3
fabricio.bernardes <fabriciob@vakinha.com.br>
2025-05-23 13:12:32 -0300

    refactor: remove unused AJAX request for keyword removal

--- app/javascript/controllers/keywords_controller.js
+++ app/javascript/controllers/keywords_controller.js
@@ -126,21 +126,5 @@ export default class extends Controller {
     const idx = event.currentTarget.dataset.keywordsIndex;
     const tag = this.keywordTagTargets.find(el => el.dataset.keywordsIndex === idx);
     if (tag) tag.remove();
-    // Se precisar remover do backend, envie um request AJAX aqui.
-    fetch(`/keywords/${idx}`, {
-      method: "DELETE",
-      headers: {
-      "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').content,
-      "Accept": "application/json"
-      }
-    })
-    .then(response => {
-      if (!response.ok) {
-      throw new Error(`Error: ${response.statusText}`)
-      }
-    })
-    .catch(error => {
-      console.error(error)
-    })
   }
 }

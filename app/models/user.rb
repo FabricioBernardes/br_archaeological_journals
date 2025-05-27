bbfa86a0c6e0e26db07a908c8cf1dde5525bde18
fabricio.bernardes <fabriciob@vakinha.com.br>
2025-05-27 13:30:38 -0300

    feat: add role management to users with migration and model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: { user: 'user', contributor: 'contributor', admin: 'admin' }, _suffix: true
+
+  def admin?
+    role == 'admin'
+  end
+
+  def contributor?
+    role == 'contributor' || admin?
+  end
+
+  def user?
+    role == 'user' || contributor || admin?
+  end
 end

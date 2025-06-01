f7cdc426e88d4a0d1ed524e9e79ea96e3a008e60
Fabrício Bernardes <fbernardes.dev@gmail.com>
2025-06-01 15:10:43 -0300

    feat: display top 10 authors with the most articles on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
+    @top_authors = Author
+                   .select('authors.*, COUNT(article_authors.id) AS articles_count')
+                   .joins(:article_authors)
+                   .group('authors.id')
+                   .order('articles_count DESC')
+                   .limit(10)
   end
 end

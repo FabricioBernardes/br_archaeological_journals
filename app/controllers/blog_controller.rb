87c7814ba03c6d3443a7cfb42f2f31c45eec3c81
Fabrício Bernardes <fbernardes.dev@gmail.com>
2025-07-07 20:33:28 -0300

    feat: implement blog functionality with index and show routes, views, and controller

---
+++ app/controllers/blog_controller.rb
@@ -0,0 +1,9 @@
+class BlogController < ApplicationController
+  def index
+    # Apenas renderiza a view estática
+  end
+
+  def show
+    # Apenas renderiza a view estática baseada no params[:id]
+  end
+end

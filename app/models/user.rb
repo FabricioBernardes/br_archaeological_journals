d9f300ac0436eb48f364a10f126ea25d46d158c8
Fabrício Bernardes <fbernardes.dev@gmail.com>
2025-09-09 21:33:47 -0300

    feat: add validations for email and password presence in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,10 +13,11 @@
 #  role                   :string
 #
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true
+  validates :password, presence: true
 
   enum role: { user: 'user', contributor: 'contributor', admin: 'admin' }, _suffix: true
 

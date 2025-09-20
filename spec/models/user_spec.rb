d9ceb527c61e0df5e62527374cd86832dd79cdf8
Fabrício Bernardes <fbernardes.dev@gmail.com>
2025-09-20 12:18:13 -0300

    feat: add enum validation for role in User model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
   end
+
+  describe 'enums' do
+    it 'defines the role enum with string values' do
+      should define_enum_for(:role)
+        .with_values(user: 'user', contributor: 'contributor', admin: 'admin')
+        .backed_by_column_of_type(:string)
+        .with_suffix
+    end
+  end
 end
